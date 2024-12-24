d96051feca47f790a8c25bdf4b24b217730ea532
ccvetter <vetrik77@gmail.com>
2024-12-24 08:54:47 -0500

    Change base url

--- frontend/src/axios_instance.ts
+++ frontend/src/axios_instance.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 var token = localStorage.getItem('access_token')
 const axiosInstance = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api/v1/',
+    baseURL: 'http://localhost:8000/api/v1/',
     withCredentials: false,
     headers: {
         "Authorization": `Bearer ${token}`
